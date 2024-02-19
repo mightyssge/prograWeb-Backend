3924e6c334b760a3a8d957381804ece61be7d319
mightyssge <118016003+mightyssge@users.noreply.github.com>
2024-02-19 17:13:24 -0500

    configurando corsheaders  y creacion de app (sqlite todavía)

---
+++ cines/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CinesConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'cines'
--- cinesBackend/settings.py
+++ cinesBackend/settings.py
@@ -37,9 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -47,6 +49,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000"
 ]
 
 ROOT_URLCONF = 'cinesBackend.urls'
